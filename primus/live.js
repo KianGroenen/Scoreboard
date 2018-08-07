74772779d5412db27365dbb011f1fbad0fd3bae7
Kian <kiangroenen@gmail.com>
2018-08-08 00:57:36 +0200

    made admin panel, added needed classes for selecting via jQuery

--- primus/live.js
+++ primus/live.js
@@ -1,16 +1,17 @@
 exports.go = function(server) {
 	const Primus = require('primus');
-	var primus = new Primus(server, {});
+	let primus = new Primus(server, {});
 
 	// your primus logic can go here
 	primus.on('connection', function connection(spark) {
 		console.log("Primus server connection 🚀");
 
 		// send a message to all connected clients
-		primus.write("Hey sparks, here is some data!");
+		//primus.write("Hey sparks, here is some data!");
 
 		// do something when we recieve data from a client
-		spark.on('data', function (data){
+		spark.on('data', function(data){
+			primus.write(data);
 			console.log("Data from spark 🛳");
 		});
 	});
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,8 +7,54 @@ var primus = Primus.connect(url, {
   }
 });
 
-primus.on('data', function(data) {
-
+primus.on('data', function message(data) {
+	// updates scores, fouls & total shots
+	if (data.score) {
+		var className = data.score[0];
+		var score = data.score[1];
+		var temp = className.split(" ");
+		var target = temp[1] + "--live";
+		$("." + target).text(score);
+	}
+	console.log(data.team);
+	if (data.team) {
+		var className = data.team[0];
+		var value = data.team[1];
+		var temp = className.split(" ");
+		var target = temp[1] + "--live";
+		$("." + target).text(value);
+	};
 });
 
 // send data to server
+
+// sends data with updated scores, fouls & total shots
+$('a').click(function () {
+	var dad = $(this).parent();
+	var target = dad.find("p");
+	var number = parseInt(target.text());
+	if ($(this).hasClass("increase")) {
+		number++;
+	} else if ($(this).hasClass("decrease")) {
+		if (number > 0) {
+			number--;
+		}
+	} else {
+	};
+	target.text(number);
+	var className = target.attr("class");
+	var numberInString = number.toString();
+	var list = [className, numberInString];
+	primus.write({score: list});
+});
+
+$('.team').keypress(function(e) {
+	if (e.keyCode == 13) {
+		var className = $(this).attr("class");
+		var value = $(this).val();
+		console.log(value);
+		var list = [className, value];
+		console.log(list);
+		primus.write({team: list});
+	};
+});
